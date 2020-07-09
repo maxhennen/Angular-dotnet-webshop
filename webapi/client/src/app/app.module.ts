193423c4bde9184520098b3f94705364fadd03eb
Max <max.hennen@planet.nl>
2020-07-09 14:42:19 +0200

    Section 10

--- webapi/client/src/app/app.module.ts
+++ webapi/client/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { ShopModule } from './shop/shop.module';
 import { HttpClientModule } from '@angular/common/http';
+import {HomeModule} from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +17,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    ShopModule
+    HomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
