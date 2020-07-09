f742fd3b456b1b95f465214ef19bde7b1e023229
Max <max.hennen@planet.nl>
2020-07-09 17:23:13 +0200

    section 12

--- webapi/client/src/app/app.module.ts
+++ webapi/client/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { ShopModule } from './shop/shop.module';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import {HomeModule} from './home/home.module';
 import {ErrorInterceptor} from './core/interceptors/error.interceptor';
+import {LoadingInterceptor} from './core/interceptors/loading.interceptors';
+
+import {NgxSpinnerModule} from 'ngx-spinner';
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,9 +22,11 @@ import {ErrorInterceptor} from './core/interceptors/error.interceptor';
     HttpClientModule,
     CoreModule,
     HomeModule,
+    NgxSpinnerModule
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true},
   ],
   bootstrap: [AppComponent],
 })
